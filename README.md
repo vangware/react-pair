5df862b2fc22a9898ebbd0a96a138679bfbe384c
Luke Shiru <hi@lshi.ru>
2022-03-22 15:23:52 -0700

    :sparkles: Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-pair
+
+Util to help with the paired hook pattern
+
+<!-- Reference -->
+
+[vangware]: https://vangware.com
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { pair } from "./pair.js";
+export type { PairWrapperProperties } from "./PairWrapperProperties.js";
+export type { RenderFunction } from "./RenderFunction.js";
